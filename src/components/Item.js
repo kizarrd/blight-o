5ebf55634b2423626cdbd8827eb56986145879d3
khhan <jacob2572502@gmail.com>
2023-11-03 20:00:09 +0800

    modularized Item component, UI for no search result

---
+++ src/components/Item.js
@@ -0,0 +1,99 @@
+import styles from "./ItemList.module.css";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+
+const shopList = {
+  slowsteadyclub: "슬로우스테디클럽",
+  beslow: "비슬로우",
+  havati: "하바티",
+  sculpstore: "스컬프스토어",
+  rhykershop: "라이커샵",
+};
+
+const getShopName = (itemId, lang = "eng") => {
+  for (const shop of Object.keys(shopList)) {
+    if (itemId.includes(shop)) {
+      if (lang === "kor") return shopList[shop];
+      else if (lang === "eng") return shop;
+    }
+  }
+};
+
+const formatPrice = (priceValue) => {
+  return new Intl.NumberFormat().format(priceValue);
+};
+
+const Item = ({data}) => {
+  const {
+    id,
+    name,
+    brand,
+    original_price,
+    sale_price,
+    sale,
+    sold_out,
+    img_url,
+    detail_page_url,
+  } = data;
+
+  return (
+    <li className={styles["itemList-li"]} key={id}>
+      <div className={styles.itemContainer}>
+        {/* <a href={detail_page_url} target="_blank" rel="noreferrer"> */}
+        <img
+          className={styles.itemImg}
+          src={"https://" + img_url}
+          alt="item"
+          // loading="lazy"
+        />
+        {/* </a> */}
+        <div className={styles.description}>
+          {/* <a href={detail_page_url} target="_blank" rel="noreferrer"> */}
+          <span className={styles.brand}>[{brand}] </span>
+          <span className={styles.item_name}>{name}</span>
+          {/* </a> */}
+        </div>
+        <div className={styles.other_info}>
+          <div className={styles.price}>
+            {sale === "True" ? (
+              <>
+                <div>
+                  <s className={styles.price__original}>
+                    {formatPrice(original_price)}원
+                  </s>
+                </div>
+                <div>
+                  <span className={styles.price__sale}>
+                    {formatPrice(sale_price)}원
+                  </span>
+                  <span className={styles.price__percentage}>
+                    {((original_price - sale_price) / original_price) *
+                      100}
+                    %
+                  </span>
+                </div>
+              </>
+            ) : (
+              <span className={styles.price_wo_sale}>
+                {formatPrice(original_price)}원
+              </span>
+            )}
+          </div>
+          <div className={styles.go_to_website}>
+            <a href={detail_page_url} target="_blank" rel="noreferrer">
+              <button className={styles.go_to_website__button}>
+                확인하러 가기{" "}
+                <OpenInNewIcon
+                  fontSize="small"
+                  className={styles.button_icon}
+                />
+              </button>
+            </a>
+            <div className={styles.website_info}>판매처: {getShopName(id, "kor")}</div>
+          </div>
+        </div>
+      </div>
+    </li>
+  )
+};
+
+export default Item;
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,10 +1,11 @@
 import { json, useLoaderData, useSearchParams } from "react-router-dom";
-import styles from "./ItemList.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { searchActions } from "../store/search-slice";
 import Pagination from "./Pagination";
 import Search from "./Search";
+import Item from "./Item";
+import styled from "@emotion/styled";
 
 const ItemList = () => {
   const itemListDataBySearch = useSelector(
@@ -19,81 +20,19 @@ const ItemList = () => {
     dispatch(searchActions.moveToThisPage({ page: page ? Number(page) : 1 }));
   }, [dispatch, searchParams]);
 
-  const shopList = {
-    slowsteadyclub: "슬로우스테디클럽",
-    beslow: "비슬로우",
-    havati: "하바티",
-    sculpstore: "스컬프스토어",
-    rhykershop: "라이커샵",
-  };
-
-  const getShopName = (itemId, lang = "eng") => {
-    for (const shop of Object.keys(shopList)) {
-      if (itemId.includes(shop)) {
-        if (lang === "kor") return shopList[shop];
-        else if (lang === "eng") return shop;
-      }
-    }
-  };
-
-  const formatPrice = (priceValue) => {
-    return new Intl.NumberFormat().format(priceValue);
-  };
-
   return (
     <>
       <Search />
-      <ul>
-        {itemListDataBySearch.map(
-          ({
-            id,
-            name,
-            brand,
-            original_price,
-            sale_price,
-            sale,
-            sold_out,
-            img_url,
-            detail_page_url,
-          }) => (
-            <li className={styles["itemList-li"]} key={id}>
-              <div className={styles.itemContainer}>
-                <a href={detail_page_url} target="_blank" rel="noreferrer">
-                  <img
-                    className={styles.itemImg}
-                    src={"https://" + img_url}
-                    alt="item"
-                    // loading="lazy"
-                  />
-                </a>
-                <div className={styles.description}>
-                  <a href={detail_page_url} target="_blank" rel="noreferrer">
-                    <span className={styles.brand}>[{brand}] </span>
-                    <span className={styles.item_name}>{name}</span>
-                  </a>
-                </div>
-                {sale === "True" ? (
-                  <div className={styles.price}>
-                    <s className={styles.price__original}>{formatPrice(original_price)}원</s>
-                    <span className={styles.price__sale}>{formatPrice(sale_price)}원</span>
-                    <span className={styles.price__percentage}>
-                      {((original_price - sale_price) / original_price) * 100}%
-                    </span>
-                  </div>
-                ) : (
-                  <div className={styles.price}>
-                    <span className={styles.price_wo_sale}>
-                      {formatPrice(original_price)}원
-                    </span>
-                  </div>
-                )}
+      {loadedData.length > 0 ? (
+        <ul>
+          {itemListDataBySearch.map((data) => (
+            <Item data={data} />
+          ))}
+        </ul>
+      ) : (
+        <div style={{padding: '30px', textAlign: 'center', lineHeight: '24px'}}>검색 결과가 없습니다.<br />철자와 띄어쓰기 등을 확인해 주세요.</div>
+      )}
 
-                <div>판매처: {getShopName(id)}</div>
-              </div>
-            </li>
-          )
-        )}
-      </ul>
       <Pagination />
     </>
   );
@@ -102,18 +41,17 @@ const ItemList = () => {
 export default ItemList;
 
 export const loader = async ({ request, params }) => {
-  // loader is not a react component. so hooks like useSearchParams cannot be used here 
+  // loader is not a react component. so hooks like useSearchParams cannot be used here
   const url = new URL(request.url);
-  const searchTerm = url.searchParams.get("search"); 
+  const searchTerm = url.searchParams.get("search");
   // console.log(searchTerm);
-  let API_URL = "http://localhost:8080/items" + (searchTerm === null ? "" : `/search?keyword=${searchTerm}`);
+  let API_URL =
+    "http://localhost:8080/items" +
+    (searchTerm === null ? "" : `/search?keyword=${searchTerm}`);
   const response = await fetch(API_URL);
   // console.log(response);
   if (!response.ok) {
-    throw json(
-      { message: "Could not fetch items" },
-      { status: 500 }
-    );
+    throw json({ message: "Could not fetch items" }, { status: 500 });
   } else {
     return response;
   }
