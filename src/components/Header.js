01331b4ec0699c08e68f530ea3f4b83f903ebad5
khhan <jacob2572502@gmail.com>
2023-11-18 23:49:41 +0800

    designing mobile UI including Nav Drawer

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,31 @@ import { Link, NavLink } from "react-router-dom";
 import Search from "./Search";
 import classes from "./Header.module.css";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import NavDrawerDesktop from "./Nav/NavDrawerDesktop";
 
 const Header = () => {
   const loading = useSelector((state) => state.loading.state);
+  const [navOpen, setNavOpen] = useState("initial");
+  const [screenIsWide, setScreenIsWide] = useState(false);
+
+  const openNavHandler = () => {
+    setNavOpen("open");
+  };
+
+  const closeNavHandler = () => {
+    setNavOpen("closed");
+  };
 
   return (
     <header className="App-header">
+      <button
+        onClick={() => {
+          setNavOpen(navOpen === "initial" || "closed" ? "open" : "closed");
+        }}
+      >
+        =
+      </button>
       <h1>
         <Link to="items">
           <span>Blight</span>
@@ -15,39 +34,48 @@ const Header = () => {
           <span className={classes.orange}>O</span>
         </Link>
       </h1>
-      {/* <Search /> */}
-      <nav>
-        <ul className={classes.navUl}>
-          <li className={classes.navList}>
-            <NavLink
-              to="/items"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Search
-            </NavLink>
-          </li>
-          <li className={classes.navList}>
-            <NavLink
-              to="/brands"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-              onClick={(e) => {
-                if(loading){
-                  e.preventDefault();
-                  alert('아직 이전 검색 결과의 데이터를 불러오는 중입니다. 잠시 후 다시 시도해 주세요');
-                  return;
-                }
-              }}
-            >
-              Brands
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
       <Search />
+      {!screenIsWide && (
+        <NavDrawerDesktop onClose={closeNavHandler} open={navOpen}>
+          <ul
+            className={`${classes.navUl} ${navOpen && classes["navUl-open"]}`}
+          >
+            <li className={classes.navList}>
+              <NavLink
+                to="/items"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+                onClick={closeNavHandler}
+              >
+                Items
+              </NavLink>
+            </li>
+            <li className={classes.navList}>
+              <NavLink
+                to="/brands"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+                onClick={(e) => {
+                  closeNavHandler();
+                  if (loading) {
+                    e.preventDefault();
+                    alert(
+                      "아직 이전 검색 결과의 데이터를 불러오는 중입니다. 잠시 후 다시 시도해 주세요"
+                    );
+                    return;
+                  }
+                }}
+              >
+                Brands
+              </NavLink>
+            </li>
+          </ul>
+        </NavDrawerDesktop>
+      )}
+
+      {/* <Search /> */}
     </header>
   );
 };
