d47f15d2f9d5f1487037cefaaa5569f0d4fe6bd1
khhan <jacob2572502@gmail.com>
2023-12-03 18:35:16 +0800

    handled execption for down/puffers

--- src/pages/NavContents.js
+++ src/pages/NavContents.js
@@ -4,22 +4,28 @@ let categoriesInFormat = [];
 for (const [bigCategory, smallCategories] of Object.entries(categories)) {
   let objectInFormat = {
     pageName: bigCategory,
-    pagePath: smallCategories.length > 1 ? '' : `/category/${bigCategory}/All`,
+    pagePath: smallCategories.length > 1 ? "" : `/category/${bigCategory}/All`,
     category: true,
     singleCategory: smallCategories.length > 1 ? false : true,
     smallCategories: [],
   };
   if (smallCategories.length > 1) {
     objectInFormat.smallCategories.push({
-      pageName: 'All',
-      pagePath: `/category/${bigCategory}/All`
+      pageName: "All",
+      pagePath: `/category/${bigCategory}/All`,
     });
-    objectInFormat.smallCategories.push(...smallCategories.map((smallCategory) => {
-      return {
-        pageName: smallCategory,
-        pagePath: `/category/${bigCategory}/${smallCategory}`,
-      };
-    }));
+    objectInFormat.smallCategories.push(
+      ...smallCategories.map((smallCategory) => {
+        const pageName = smallCategory;
+        const pagePath = `/category/${bigCategory}/${
+          smallCategory === "Down/Puffers" ? "DownPuffers" : smallCategory
+        }`;
+        return {
+          pageName,
+          pagePath,
+        };
+      })
+    );
   }
   categoriesInFormat.push(objectInFormat);
 }
